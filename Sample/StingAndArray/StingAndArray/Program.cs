93967e5e22d257177b53d4d191a15aafc77a5b59
dnlwrn01 <78996771+dnlwrn01@users.noreply.github.com>
2021-06-10 12:16:33 -0500

    Create basic String/Array/List

---
+++ Sample/StingAndArray/StingAndArray/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StingAndArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vehicleType = new string[] { "Car", "Truck", "SUV", "Van" };
+            int[] vehicleYear = new int[] { 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021 };
+            List<string> vehicleMake = new List<string>() { "Ford", "dodge", "chevy"};
+
+            Console.WriteLine(vehicleMake[2]);
+
+            Console.WriteLine("Welcome to a thing that shows you your car. \n");
+
+            Console.WriteLine("Please select a vehicle type: ");
+            Console.WriteLine("1. CAR ");
+            Console.WriteLine("2. TRUCK ");
+            Console.WriteLine("3. SUV ");
+            Console.WriteLine("4. VAN ");
+
+            int userVehicleType = Convert.ToInt32(Console.ReadLine());
+
+            switch(userVehicleType)
+            {
+                case 1:
+                    Console.WriteLine(vehicleType[0]);
+                    break;
+
+                case 2:
+                    Console.WriteLine(vehicleType[1]);
+                    break;
+
+                case 3:
+                    Console.WriteLine(vehicleType[2]);
+                    break;
+
+                case 4:
+                    Console.WriteLine(vehicleType[3]);
+                    break;
+            }
+            
+
+            Console.WriteLine("Please select a vehicle year: ");
+            Console.WriteLine("1. 2010 ");
+            Console.WriteLine("2. 2011 ");
+            Console.WriteLine("3. 2012 ");
+            Console.WriteLine("4. 2013 ");
+            Console.WriteLine("5. 2014 ");
+            Console.WriteLine("6. 2015 ");
+            Console.WriteLine("7. 2016 ");
+            Console.WriteLine("8. 2017 ");
+            Console.WriteLine("9. 2018 ");
+            Console.WriteLine("10. 2019 ");
+            Console.WriteLine("11. 2020 ");
+            Console.WriteLine("12. 2021 ");
+
+            int userVehicleYear = Convert.ToInt32(Console.ReadLine());
+
+            switch (userVehicleYear)
+            {
+                case 1:
+                    Console.WriteLine(vehicleYear[0]);
+                    break;
+
+                case 2:
+                    Console.WriteLine(vehicleYear[1]);
+                    break;
+
+                case 3:
+                    Console.WriteLine(vehicleYear[2]);
+                    break;
+
+                case 4:
+                    Console.WriteLine(vehicleYear[3]);
+                    break;
+
+                case 5:
+                    Console.WriteLine(vehicleYear[4]);
+                    break;
+
+                case 6:
+                    Console.WriteLine(vehicleYear[5]);
+                    break;
+
+                case 7:
+                    Console.WriteLine(vehicleYear[6]);
+                    break;
+
+                case 8:
+                    Console.WriteLine(vehicleYear[7]);
+                    break;
+
+                case 9:
+                    Console.WriteLine(vehicleYear[8]);
+                    break;
+
+                case 10:
+                    Console.WriteLine(vehicleYear[9]);
+                    break;
+
+                case 11:
+                    Console.WriteLine(vehicleYear[10]);
+                    break;
+
+                case 12:
+                    Console.WriteLine(vehicleYear[11]);
+                    break;
+            }
+            Console.Read();
+
+
+
+        }
+    }
+}   
---
+++ Sample/String Concatenation/String Concatenation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Concatenation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //inputs
+            Console.WriteLine("What is your first name?");
+            string fName = Console.ReadLine();
+            Console.WriteLine("What is your middle name?");
+            string mName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            string lName = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is your favorite hobby? (1/3)");
+            StringBuilder bio = new StringBuilder(Console.ReadLine() + ", ");
+            Console.WriteLine("What is your favorite hobby? (2/3)");
+            bio.Append(Console.ReadLine() + ", ");
+            Console.WriteLine("What is your favorite hobby? (3/3)");
+            bio.Append("& " + Console.ReadLine() + ".");
+
+            //string manipulation
+            string fullName = (fName + " " + mName + " " + lName);
+            string initials = fName.Substring(0, 1) + lName.Substring(0, 1);
+            initials = initials.ToUpper();
+
+            //display results
+            Console.WriteLine("\n\nName: " + fullName + "\nInitials: " + initials + "\nAge:" + age + "\nHobbies Include: " + bio);
+
+            Console.Read();
+        }
+    }
+}
