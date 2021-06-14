72c5e3f825dc299693a68e70add46a4b7c607027
dnlwrn01 <78996771+dnlwrn01@users.noreply.github.com>
2021-06-14 17:20:09 -0500

    create MoreMethods project

---
+++ Sample/MoreMethods/MoreMethods/Math.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreMethods
+{
+    class Math
+    {
+        //int method for addition
+        public int AddNum(int userNum)
+        {
+            int result = userNum + 100;
+            return result;
+        }
+
+        //decimal method for addition
+        public int AddNum(decimal userDecimalNum)
+        {
+            decimal temp = userDecimalNum + 1200;
+            int result = Convert.ToInt32(temp);
+            return result;
+        }
+
+        //string method for addition
+        public int AddNum(string userStringNum)
+        {
+            int temp = int.Parse(userStringNum);
+            int result = temp + 5;
+            return result;
+        }
+    }
+}
---
+++ Sample/MoreMethods/MoreMethods/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the class
+            Math math = new Math();
+
+            //user int input/ calc / output
+            Console.WriteLine("Enter a number:");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            int sum = math.AddNum(userNum);
+            Console.WriteLine(sum);
+
+            //user decimal input/ calc / output
+            Console.WriteLine("Enter a decimal number:");
+            decimal userDecimalNum = Convert.ToDecimal(Console.ReadLine());
+            int decimalSum = math.AddNum(userDecimalNum);
+            Console.WriteLine(decimalSum);
+
+            //user string input/ calc / output
+            Console.WriteLine("Enter a number as a string:");
+            string userStringNum = Convert.ToString(Console.ReadLine());
+            int stringSum = math.AddNum(userStringNum);
+            Console.WriteLine(stringSum);
+
+
+            Console.Read();
+        }
+    }
+}
