7a83acf10a48eeda6871c541edc6c3899e8590d7
dnlwrn01 <78996771+dnlwrn01@users.noreply.github.com>
2021-06-15 12:35:05 -0500

    create SalesTaxCalc
    Simple console app for WI sales tax

---
+++ Sample/Method_Class/Method_Class/Math.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class
+{
+    class Math
+    {
+        //find total after sales tax
+        public static void SalesTax(decimal taxPercent, decimal itemCost)
+        {
+            //calc tax and total
+            taxPercent = (taxPercent / 100);
+            decimal totalTax = taxPercent * itemCost;
+            decimal total = totalTax + itemCost;
+
+            //display results
+            Console.WriteLine("Item Cost: " + itemCost);
+            Console.WriteLine("Tax: " + totalTax);
+            Console.WriteLine("Total: " + total);
+            return;
+        }
+
+    }
+}
---
+++ Sample/Method_Class/Method_Class/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate claass
+            Math math = new Math();
+
+            //get item cost for user
+            Console.WriteLine("FIND WI ITEM COST AFTER TAX: \nWhat is the cost of the item?");
+            decimal itemCost = Convert.ToDecimal(Console.ReadLine());
+
+            //pass itemCost + tax percent (by name)
+            Math.SalesTax(taxPercent: 5, itemCost);
+
+            Console.Read();
+        }
+    }
+}
