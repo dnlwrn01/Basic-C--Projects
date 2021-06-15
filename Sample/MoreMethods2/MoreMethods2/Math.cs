e20787993cf2eefe8ad5c538f4845c97716b0ec4
dnlwrn01 <78996771+dnlwrn01@users.noreply.github.com>
2021-06-14 21:34:16 -0500

    Create methods with optional parameters

---
+++ Sample/MoreMethods2/MoreMethods2/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreMethods2
+{
+    class Math
+    {
+        //method with 2 parameters (1 optional)
+        public int AddNum(int one, int two = 0)
+        {
+            int result = one + two;
+            return result;
+        }
+    }
+}
---
+++ Sample/MoreMethods2/MoreMethods2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreMethods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Math math = new Math();
+
+            //get user num 1
+            Console.WriteLine("Enter a number:");
+            int one = Convert.ToInt32(Console.ReadLine());
+
+            //ask for second num
+            Console.WriteLine("Enter another number? [y/n]:");
+            string secondNum = Convert.ToString(Console.ReadLine());
+            int result = 0;
+            
+            if ((secondNum == "Y") || (secondNum == "y"))
+            {
+                //get num2
+                int two = Convert.ToInt32(Console.ReadLine());
+                result = math.AddNum(one, two);
+            }
+            else if ((secondNum == "N") || (secondNum == "n"))
+            {
+                result = math.AddNum(one);
+            }
+            else
+            {
+                Console.WriteLine("Not an option. Try Again Later.");
+            }
+
+            //display result
+            Console.WriteLine(result);
+            Console.Read();
+
+        }
+    }
+}
