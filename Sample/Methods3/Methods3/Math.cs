75e74b2e6d0a321c4e6a4f7e78fd7978f9d995ff
dnlwrn01 <78996771+dnlwrn01@users.noreply.github.com>
2021-06-15 13:17:50 -0500

    Create Methods3

---
+++ Sample/Methods3/Methods3/Math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods3
+{
+    class Math
+    {
+        public static void DisplayPromt()
+        {
+            Console.WriteLine("WELCOME TO THIS");
+            Console.WriteLine("Enter a number:");
+        }
+
+        public static int Divide(int userNum)
+        {
+            int result = userNum / 2;
+            return result;
+        }
+
+        public static int Divide(int userNum, int DivideByNum = 1)
+        {
+            int result = userNum / DivideByNum;
+            return result;
+        }
+
+    }
+}
---
+++ Sample/Methods3/Methods3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Math math = new Math();
+
+            Math.DisplayPromt();
+
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Would you like to divide this number by anything other than '2'? ['Y' / 'N']");
+            string secondNum = Convert.ToString(Console.ReadLine());
+
+            int result = 0;
+
+            if ((secondNum == "y") || (secondNum == "Y"))
+            {
+                Console.WriteLine("Enter that number:");
+                int userNum2 = Convert.ToInt32(Console.ReadLine());
+                result = Math.Divide(userNum, userNum2);
+            }
+            else if ((secondNum == "n") || (secondNum == "N"))
+            {
+                result = Math.Divide(userNum);
+            }
+            else
+            {
+                Console.WriteLine("This is not an option. Try Again later.");
+            }
+
+            Console.WriteLine(result);
+
+            Console.Read();
+        }
+    }
+}
