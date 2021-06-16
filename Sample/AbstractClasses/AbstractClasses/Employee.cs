b1b80732559326a69218dd0c748d6412ba75f40f
dnlwrn01 <78996771+dnlwrn01@users.noreply.github.com>
2021-06-16 13:48:12 -0500

    add new Interface

--- Sample/AbstractClasses/AbstractClasses/Employee.cs
+++ Sample/AbstractClasses/AbstractClasses/Employee.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AbstractClasses
 {
-    public class Employee : Person
+    public class Employee : Person, IQuittable
     {
         //override method
         public override void SayName()
