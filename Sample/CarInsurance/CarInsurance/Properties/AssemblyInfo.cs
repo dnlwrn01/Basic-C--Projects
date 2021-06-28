4428e3abf56f11cae95d3992daf1def760642c5d
Daniel Warren <78996771+dnlwrn01@users.noreply.github.com>
2021-06-27 23:19:46 -0500

    Create CarInsuranceApp

--- Sample/CarInsurance/CarInsurance/Properties/AssemblyInfo.cs
+++ Sample/CarInsurance/CarInsurance/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7f0dd273-2fca-4775-9555-0b395bf390fb")]
+[assembly: Guid("5eeeb510-8ecb-408b-90db-615f48706f11")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
